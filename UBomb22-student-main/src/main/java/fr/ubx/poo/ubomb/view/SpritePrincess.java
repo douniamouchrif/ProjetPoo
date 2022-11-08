27f700b048bdcabcf2d9b30b000af893e7c829f5
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-11-08 11:00:30 +0100

    Create SpritePrincess.java

---
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/view/SpritePrincess.java
@@ -0,0 +1,8 @@
+package fr.ubx.poo.ubomb.view;
+
+import fr.ubx.poo.ubomb.go.character.Player;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+
+public class SpritePrincess {
+}
