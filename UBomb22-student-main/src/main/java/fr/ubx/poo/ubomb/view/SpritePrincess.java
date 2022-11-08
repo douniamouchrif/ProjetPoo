e64945b11c3edd51cabd5b3c88fb2fae6796ca38
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-11-08 11:32:49 +0100

    Update SpritePrincess.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/view/SpritePrincess.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/view/SpritePrincess.java
@@ -1,8 +1,20 @@
 package fr.ubx.poo.ubomb.view;
 
-import fr.ubx.poo.ubomb.go.character.Player;
+import fr.ubx.poo.ubomb.go.GameObject;
+import fr.ubx.poo.ubomb.go.character.Princess;
+import fr.ubx.poo.ubomb.go.decor.Stone;
+import fr.ubx.poo.ubomb.go.decor.Tree;
+import fr.ubx.poo.ubomb.go.decor.bonus.Key;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
-public class SpritePrincess {
+import static fr.ubx.poo.ubomb.view.ImageResource.*;
+
+public final class SpritePrincess {
+    public static Sprite create(Pane layer, GameObject gameObject) {
+        if (gameObject instanceof Princess)
+            return new Sprite(layer, PRINCESS.getImage(), gameObject);
+
+        throw new RuntimeException("Unsupported sprite for princess " + gameObject);
+    }
 }
