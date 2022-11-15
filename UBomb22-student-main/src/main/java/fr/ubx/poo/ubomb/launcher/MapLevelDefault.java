9b11b33e6617f52a0511b21b46c2661f19647bd5
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-11-15 10:27:08 +0100

    Update MapLevelDefault.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/MapLevelDefault.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/MapLevelDefault.java
@@ -4,10 +4,10 @@ import static fr.ubx.poo.ubomb.launcher.Entity.*;
 
 public class MapLevelDefault extends MapLevel {
     private final static Entity[][] level1 = {
-            {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty},
+            {Empty, Empty, Empty, Empty, Empty, Empty, Box, Empty, Empty, Empty, Empty, Empty},
             {Empty, Empty, Stone, Empty, Stone, Empty, Stone, Stone, Stone, Stone, Empty, Empty},
             {Empty, Empty, Empty, Empty, Stone, Empty, Stone, Empty, Empty, Stone, Empty, Empty},
-            {Empty, Empty, Empty, Empty, Stone, Empty, Stone, Empty, Empty, Stone, Empty, Empty},
+            {Empty, Empty, Empty, Empty, Stone, Empty, Stone, Empty, Box, Stone, Empty, Empty},
             {Empty, Empty, Empty, Empty, Stone, Stone, Stone, Empty, Empty, Empty, Empty, Empty},
             {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Key, Empty, Stone, Empty, Empty},
             {Empty, Tree, Empty, Tree, Empty, Empty, Empty, Empty, Empty, Stone, Empty, Empty},
@@ -16,7 +16,7 @@ public class MapLevelDefault extends MapLevel {
             {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty},
             {Stone, Stone, Stone, Stone, Stone, Empty, Empty, Empty, Stone, Stone, Empty, Stone},
             {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty},
-            {Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty}
+            {Empty, Empty, Empty, Box, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty}
     };
     private final static int width = 12;
     private final static int height = 13;
