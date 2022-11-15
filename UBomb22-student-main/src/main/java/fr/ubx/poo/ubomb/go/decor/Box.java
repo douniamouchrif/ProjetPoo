a47acba3abb1ad9adaef940535cb54be6cfb5879
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-11-15 11:24:36 +0100

    Update Box.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
@@ -1,9 +1,23 @@
 package fr.ubx.poo.ubomb.go.decor;
 
+import fr.ubx.poo.ubomb.game.Direction;
+import fr.ubx.poo.ubomb.game.Game;
 import fr.ubx.poo.ubomb.game.Position;
+import fr.ubx.poo.ubomb.go.GameObject;
+import fr.ubx.poo.ubomb.go.Movable;
 
-public class Box extends Decor {
+public class Box extends Decor implements Movable{
     public Box(Position position) {
         super(position);
+    }
+
+    @Override
+    public boolean canMove(Direction direction) {
+        return false;
+    }
+
+    @Override
+    public void doMove(Direction direction) {
+
     }
 }
