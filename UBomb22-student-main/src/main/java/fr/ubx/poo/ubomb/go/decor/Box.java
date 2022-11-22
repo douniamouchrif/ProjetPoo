f1fa89a6d3dd57b2287761a84ace8aacfe26733d
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-11-22 09:45:05 +0100

    Update Box.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Box.java
@@ -5,6 +5,7 @@ import fr.ubx.poo.ubomb.game.Game;
 import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.GameObject;
 import fr.ubx.poo.ubomb.go.Movable;
+import fr.ubx.poo.ubomb.go.decor.bonus.Bonus;
 
 public class Box extends Decor implements Movable{
     public Box(Position position) {
@@ -13,56 +14,11 @@ public class Box extends Decor implements Movable{
 
     @Override
     public boolean canMove(Direction direction) {
-        if(direction == Direction.UP){
-            if (getPosition().y() <= 0){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x(),getPosition().y()-1)) instanceof Tree ){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x(),getPosition().y()-1)) instanceof Stone ){
-                return false;
-            }
-        }
-        if(direction == Direction.DOWN){
-            if (getPosition().y() >= game.grid().height()-1){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x(),getPosition().y()+1)) instanceof Tree ){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x(),getPosition().y()+1)) instanceof Stone ){
-                return false;
-            }
-        }
-        if(direction == Direction.LEFT){
-            if (getPosition().x() <= 0){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x()-1,getPosition().y())) instanceof Tree ){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x()-1,getPosition().y())) instanceof Stone ){
-                return false;
-            }
-        }
-        if(direction == Direction.RIGHT){
-            if (getPosition().x() >= game.grid().width()-1){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x()+1,getPosition().y())) instanceof Tree ){
-                return false;
-            }
-            if (game.grid().get(new Position(getPosition().x()+1,getPosition().y())) instanceof Stone ){
-                return false;
-            }
-        }
 
         return true;
     }
 
     @Override
     public void doMove(Direction direction) {
-
     }
 }
