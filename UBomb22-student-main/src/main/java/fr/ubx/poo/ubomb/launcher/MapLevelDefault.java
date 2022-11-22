ff828396063068348f62f639f62be4a6eee1d8d9
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-11-22 11:31:00 +0100

    Update MapLevelDefault.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/MapLevelDefault.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/MapLevelDefault.java
@@ -4,8 +4,8 @@ import static fr.ubx.poo.ubomb.launcher.Entity.*;
 
 public class MapLevelDefault extends MapLevel {
     private final static Entity[][] level1 = {
-            {Empty, Empty, Empty, Empty, Empty, Empty, Box, Empty, Empty, Empty, Empty, Empty},
-            {Empty, Empty, Stone, Empty, Stone, Empty, Stone, Stone, Stone, Stone, Empty, Empty},
+            {Empty, Key, Empty, Empty, Empty, Empty, Box, Empty, Empty, Empty, Empty, Empty},
+            {Key, Empty, Stone, Empty, Stone, Empty, Stone, Stone, Stone, Stone, Empty, Empty},
             {Empty, Empty, Empty, Empty, Stone, Empty, Stone, Empty, Empty, Stone, Empty, Empty},
             {Empty, Empty, Empty, Empty, Stone, Empty, Stone, Empty, Box, Stone, Empty, Empty},
             {Empty, Empty, Empty, Empty, Stone, Stone, Stone, Empty, Empty, Empty, Empty, Empty},
