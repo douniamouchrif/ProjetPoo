d83cee437f084b8e6e6b5a89e8844c0ac22685d0
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-11-29 12:25:03 +0100

    Create BombNumberInc.java

---
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/bonus/BombNumberInc.java
@@ -0,0 +1,19 @@
+package fr.ubx.poo.ubomb.go.decor.bonus;
+
+import fr.ubx.poo.ubomb.game.Position;
+import fr.ubx.poo.ubomb.go.character.Player;
+
+public class BombNumberInc extends Bonus {
+
+    public BombNumberInc(Position position) {
+        super(position);
+    }
+
+    @Override
+    public void explode() {}
+
+    @Override
+    public void takenBy(Player player) {
+        player.take(this);
+    }
+}
