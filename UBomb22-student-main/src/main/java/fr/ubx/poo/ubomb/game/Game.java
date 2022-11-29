2886b5b21a9c04f1461c5c79e79bf07142895219
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-11-29 12:24:52 +0100

    Update Game.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/game/Game.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/game/Game.java
@@ -1,6 +1,7 @@
 package fr.ubx.poo.ubomb.game;
 
 import fr.ubx.poo.ubomb.go.GameObject;
+import fr.ubx.poo.ubomb.go.character.Monster;
 import fr.ubx.poo.ubomb.go.character.Player;
 
 import java.util.LinkedList;
@@ -10,12 +11,14 @@ public class Game {
 
     private final Configuration configuration;
     private final Player player;
+    private final Monster monster;
     private final Grid grid;
 
     public Game(Configuration configuration, Grid grid) {
         this.configuration = configuration;
         this.grid = grid;
         player = new Player(this, configuration.playerPosition());
+        monster = new Monster(this, configuration.monsterPosition());
     }
 
     public Configuration configuration() {
@@ -27,6 +30,8 @@ public class Game {
         List<GameObject> gos = new LinkedList<>();
         if (player().getPosition().equals(position))
             gos.add(player);
+        if (monster().getPosition().equals(position))
+            gos.add(monster);
         return gos;
     }
 
@@ -38,5 +43,8 @@ public class Game {
         return this.player;
     }
 
+    public Monster monster() {
+        return this.monster;
+    }
 
 }
