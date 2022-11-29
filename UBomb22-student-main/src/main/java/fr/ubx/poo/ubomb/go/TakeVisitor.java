0dd17e08b884ed5a7bf553e56b77386cbbd57279
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-11-29 12:25:14 +0100

    Update TakeVisitor.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/TakeVisitor.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/TakeVisitor.java
@@ -6,6 +6,9 @@ import fr.ubx.poo.ubomb.go.decor.bonus.*;
 public interface TakeVisitor {
     // Key
     default void take(Key key) {}
-
-
+    default void take(Heart heart) {}
+    default void take(BombNumberInc bombnumberinc) {}
+    default void take(BombNumberDec bombnumberdec) {}
+    default void take(BombRangeInc bombrangeinc) {}
+    default void take(BombRangeDec bombrangedec) {}
 }
