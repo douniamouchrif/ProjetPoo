8fb062d96f9f739cbd11051f7724b1f0acd77753
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-12-28 22:50:35 +0100

    Update Decor.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Decor.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Decor.java
@@ -1,11 +1,14 @@
 package fr.ubx.poo.ubomb.go.decor;
 
+
+import fr.ubx.poo.ubomb.game.Direction;
 import fr.ubx.poo.ubomb.game.Game;
 import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.GameObject;
+import fr.ubx.poo.ubomb.go.Movable;
 import fr.ubx.poo.ubomb.go.character.Player;
 
-public abstract class Decor extends GameObject {
+public abstract class Decor extends GameObject implements Movable{
 
     public Decor(Game game, Position position) {
         super(game, position);
@@ -19,5 +22,7 @@ public abstract class Decor extends GameObject {
     public boolean walkableBy(Player player){
         return player.walk(this);
     }*/
+    abstract public boolean canMove(Direction direction);
+    abstract public void doMove(Direction direction);
 
 }
