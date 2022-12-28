530cf338de7194b27833021fad6e39099a1da0d3
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-12-28 15:20:44 +0100

    Update Configuration.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/game/Configuration.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/game/Configuration.java
@@ -1,5 +1,5 @@
 package fr.ubx.poo.ubomb.game;
 
-public record Configuration(Position playerPosition, Position monsterPosition, int bombBagCapacity, int playerLives, long playerInvisibilityTime,
+public record Configuration(Position playerPosition, Position monsterPosition, int bombBagCapacity, int playerLives, int keys, int range, long playerInvisibilityTime,
                             int monsterVelocity, long monsterInvisibilityTime) {
 }
