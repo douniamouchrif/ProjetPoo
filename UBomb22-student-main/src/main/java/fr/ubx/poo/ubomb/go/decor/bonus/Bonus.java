27173f49de7af00af46f9732744ca534ba94e473
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-12-28 22:50:38 +0100

    Update Bonus.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/bonus/Bonus.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/bonus/Bonus.java
@@ -4,6 +4,7 @@
 
 package fr.ubx.poo.ubomb.go.decor.bonus;
 
+import fr.ubx.poo.ubomb.game.Direction;
 import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.Takeable;
 import fr.ubx.poo.ubomb.go.character.Player;
@@ -22,6 +23,13 @@ public abstract class Bonus extends Decor implements Takeable {
     @Override
     public void explode() {
         remove();
+    }
+
+    public boolean canMove(Direction direction){
+        return false;
+    }
+    public void doMove(Direction direction){
+
     }
 
 }
