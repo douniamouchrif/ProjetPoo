1ed2a2658c6b138f764372860b1cc6266198f9bb
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2022-12-28 22:50:28 +0100

    Update Tree.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Tree.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Tree.java
@@ -3,10 +3,18 @@
  */
 
 package fr.ubx.poo.ubomb.go.decor;
+import fr.ubx.poo.ubomb.game.Direction;
 import fr.ubx.poo.ubomb.game.Position;
 
 public class Tree extends Decor {
     public Tree(Position position) {
         super(position);
+    }
+
+    public boolean canMove(Direction direction){
+        return false;
+    }
+    public void doMove(Direction direction){
+
     }
 }
