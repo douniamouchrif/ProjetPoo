79542c8bb211c667f712f1c47301c7fd451bf725
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-12-29 22:35:47 +0100

    Update Monster.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/character/Monster.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/character/Monster.java
@@ -1,18 +1,21 @@
 package fr.ubx.poo.ubomb.go.character;
 
-import fr.ubx.poo.ubomb.engine.Timer;
+//import fr.ubx.poo.ubomb.engine.Timer;
 import fr.ubx.poo.ubomb.game.Direction;
 import fr.ubx.poo.ubomb.game.Game;
 import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.GameObject;
 import fr.ubx.poo.ubomb.go.Movable;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Monster extends GameObject implements Movable {
 
     private Direction direction;
     private long lastMove;
 
-    Timer time = new Timer(2000000000);;
+    //Timer time = new Timer(2000000000);
 
     //private int lives;
 
@@ -57,18 +60,48 @@ public class Monster extends GameObject implements Movable {
         }
     }*/
 
-    public void update(long now) {
+    /*public void update(long now) {
+        time.start();
         lastMove = now + 10 * 1000000000L / game.configuration().monsterVelocity();
-        if (now >= lastMove) {
-            requestMove(Direction.random());
-            lastMove += (10 * 1000000000L / game.configuration().monsterVelocity());
+        if (now < lastMove) {
+            if (canMove(direction)) {
+                doMove(direction);
+            } else {
+                direction = Direction.random();
+            }
         }
+        System.out.println("time ...");
+        requestMove(Direction.random());
+        lastMove += 10 * 1000000000L / game.configuration().monsterVelocity();
+        time.update(now);
+    }*/
+
+    /*public void update(long now) {
+        lastMove = now + 10 * 1000000000L / game.configuration().monsterVelocity();
+        if (now < lastMove) {
+            if (canMove(direction)) {
+                doMove(direction);
+            } else {
+                direction = Direction.random();
+            }
+        }
+        System.out.println("time ...");
+        requestMove(Direction.random());
+        lastMove += 10 * 1000000000L / game.configuration().monsterVelocity();
+    }*/
+
+    public void update(long now) {
         if (canMove(direction)) {
             doMove(direction);
+            lastMove = now + 10 * 1000000000L / game.configuration().monsterVelocity();
+            if (now >= lastMove){
+                System.out.println("time ...");
+                requestMove(Direction.random());
+                lastMove += (10 * 1000000000L / game.configuration().monsterVelocity());
+            }
         } else {
             direction = Direction.random();
         }
-        time.update(now);
     }
 
     public void requestMove(Direction direction) {
