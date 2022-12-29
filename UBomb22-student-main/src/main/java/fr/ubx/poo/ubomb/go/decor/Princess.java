cb9fab0628cd748e7f4168cf81730a61510fb3d7
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-12-29 19:34:35 +0100

    Create Princess.java

---
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Princess.java
@@ -0,0 +1,31 @@
+package fr.ubx.poo.ubomb.go.decor;
+
+import fr.ubx.poo.ubomb.game.Direction;
+import fr.ubx.poo.ubomb.game.Game;
+import fr.ubx.poo.ubomb.game.Position;
+import fr.ubx.poo.ubomb.go.GameObject;
+import fr.ubx.poo.ubomb.go.character.Player;
+
+public class Princess extends Decor {
+
+    public Princess(Game game, Position position) {
+        super(game, position);
+    }
+
+    public Princess(Position position) {
+        super(position);
+    }
+
+    @Override
+    public boolean walkableBy(Player player) {
+        return true;
+    }
+    @Override
+    public boolean canMove(Direction direction){
+        return false;
+    }
+    @Override
+    public void doMove(Direction direction){
+
+    }
+}
