7b245ab8c1428d513131a962498d2e8b7a095333
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2022-12-29 19:34:50 +0100

    Update WalkVisitor.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/WalkVisitor.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/WalkVisitor.java
@@ -1,13 +1,13 @@
 package fr.ubx.poo.ubomb.go;
 
-import fr.ubx.poo.ubomb.go.character.Princess;
+
 import fr.ubx.poo.ubomb.go.decor.bonus.*;
 import fr.ubx.poo.ubomb.go.decor.*;
 
 // Double dispatch visitor pattern
 public interface WalkVisitor {
 
-    default boolean walk(Princess princess) {return false;}
+    //default boolean walk(Princess princess) {return false;}
     default boolean walk(Box box) {return false;}
 
     //default boolean walk(Decor decor) {return false;}
