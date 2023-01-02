8555c02aa5b13bb80621afea21946f13e141739e
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2023-01-02 17:05:57 +0100

    Update BombRangeInc.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/bonus/BombRangeInc.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/bonus/BombRangeInc.java
@@ -10,9 +10,6 @@ public class BombRangeInc extends Bonus {
     }
 
     @Override
-    public void explode() {}
-
-    @Override
     public void takenBy(Player player) {
         player.take(this);
     }
