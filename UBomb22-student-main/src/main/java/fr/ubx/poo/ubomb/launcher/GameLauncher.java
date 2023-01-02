6e0cbc35e890c13e541964d0fb7252fd2b48b8bd
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2023-01-02 20:51:57 +0100

    Update GameLauncher.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/GameLauncher.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/GameLauncher.java
@@ -8,7 +8,12 @@ import fr.ubx.poo.ubomb.game.Position;
 public class GameLauncher {
 
     public static Game load() {
-        Configuration configuration = new Configuration(new Position(0, 0), new Position(10, 10),3, 5, 0, 1, 4000, 10, 1000);
+        Position[] position = new Position[3];
+        position[0] = new Position(10,10);
+        position[1] = new Position(1,7);
+        position[2] = new Position(8,2);
+        Configuration configuration = new Configuration(new Position(0, 0), position, 3, 5, 0, 1, 4000, 10, 1000);
+
         return new Game(configuration, new Level(new MapLevelDefault()));
     }
 
