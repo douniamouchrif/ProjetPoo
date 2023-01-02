2dce1559459256ba05b335498441e96449bc320e
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2023-01-02 20:51:54 +0100

    Update Monster.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/character/Monster.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/character/Monster.java
@@ -1,14 +1,10 @@
 package fr.ubx.poo.ubomb.go.character;
 
-import fr.ubx.poo.ubomb.engine.Timer;
 import fr.ubx.poo.ubomb.game.Direction;
 import fr.ubx.poo.ubomb.game.Game;
 import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.GameObject;
 import fr.ubx.poo.ubomb.go.Movable;
-
-//import java.util.Timer;
-//import java.util.TimerTask;
 
 public class Monster extends GameObject implements Movable {
 
