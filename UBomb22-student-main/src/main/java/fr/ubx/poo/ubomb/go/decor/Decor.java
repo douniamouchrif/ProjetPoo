d51fb5f78071403072f7cead76e048f17bccb69a
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2023-01-03 01:24:15 +0100

    Update Decor.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Decor.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/go/decor/Decor.java
@@ -6,7 +6,6 @@ import fr.ubx.poo.ubomb.game.Game;
 import fr.ubx.poo.ubomb.game.Position;
 import fr.ubx.poo.ubomb.go.GameObject;
 import fr.ubx.poo.ubomb.go.Movable;
-import fr.ubx.poo.ubomb.go.character.Player;
 
 public abstract class Decor extends GameObject implements Movable{
 
