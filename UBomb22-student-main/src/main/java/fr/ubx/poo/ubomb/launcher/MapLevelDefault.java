23f643ed9308d0764bd55c31f56598b687d061b4
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2023-01-03 01:24:29 +0100

    Update MapLevelDefault.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/MapLevelDefault.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/launcher/MapLevelDefault.java
@@ -1,11 +1,13 @@
 package fr.ubx.poo.ubomb.launcher;
 
+import fr.ubx.poo.ubomb.go.decor.DoorClosed;
+
 import static fr.ubx.poo.ubomb.launcher.Entity.*;
 
 public class MapLevelDefault extends MapLevel {
     private final static Entity[][] level1 = {
             {Empty, Empty, Empty, Empty, Empty, Empty, Box, Empty, Empty, Empty, Empty, Empty},
-            {Empty, Empty, Stone, Empty, Stone, Empty, Stone, Stone, Stone, Stone, Empty, Empty},
+            {Empty, Empty, Stone, Empty, Stone, Empty, Stone, Stone, Stone, Stone, Empty, DoorNextClosed},
             {Empty, Empty, Empty, Empty, Stone, Empty, Stone, Empty, Empty, Stone, Empty, Empty},
             {Empty, BombRangeInc, Empty, Empty, Stone, Empty, Stone, Empty, Box, Stone, Empty, Empty},
             {Empty, Empty, Empty, Empty, Stone, Stone, Stone, Empty, Empty, Empty, Empty, Empty},
