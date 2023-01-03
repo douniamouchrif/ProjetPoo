1518555c7fc4122971b7598b7745e5e4f8609064
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2023-01-03 01:24:39 +0100

    Update SpriteFactory.java

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/view/SpriteFactory.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/view/SpriteFactory.java
@@ -35,6 +35,10 @@ public final class SpriteFactory {
             return new Sprite(layer, BONUS_BOMB_RANGE_INC.getImage(), gameObject);
         if (gameObject instanceof BombRangeDec)
             return new Sprite(layer, BONUS_BOMB_RANGE_DEC.getImage(), gameObject);
+        if (gameObject instanceof DoorClosed)
+            return new Sprite(layer, DOOR_CLOSED.getImage(), gameObject);
+        if (gameObject instanceof DoorOpened)
+            return new Sprite(layer, DOOR_OPENED.getImage(), gameObject);
 
         throw new RuntimeException("Unsupported sprite for decor " + gameObject);
     }
